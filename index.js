e36ae0afa3920bb17ee0472d44d33ea94f27c635
Upendra Singh <usingh8@asu.edu>
2018-02-03 17:08:58 -0700

    modified index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'production'){
   // to server index.html file
   const path  = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
 }
