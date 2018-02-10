8aad865e186f9f3d347396abd3f65f8595a12586
Upendra Singh <usingh8@asu.edu>
2018-02-10 14:03:18 -0700

    Survey Code added in backend

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,8 @@ import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
-
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {},applyMiddleware(reduxThunk));
 
